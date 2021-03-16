ed576482fecefc441d098b83862b19e3d7cccf94
Dirakon <609700zxc@mail.ru>
2021-03-16 17:36:44 +0500

    Now logic is done, settings are added, what's left is visuals.

--- main.py
+++ main.py
@@ -13,16 +13,41 @@ import time
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
-#driver.minimize_window()
+
+file = open('settings.txt','r',encoding='utf-8')
+
+all_settings = file.read().split('\n')
+
+urls = {}
+
+for setting in all_settings:
+    setting = [i.strip(' ') for i in setting.split('=')]
+    if 'url' in setting[0] and len(setting) > 2:
+        urls[setting[1].split("'")[1]]=setting[2].split("'")[1]
+
+print("Выберите предмет.")
+ind = 1
+for i in urls:
+    print(ind,i)
+    ind+=1
+
+choice = 0
+while choice < 1 or choice >len(urls):
+    choice = int(input("Номер предмета: "))
+
+ind = 1
+for i in urls:
+    if ind == choice:
+        url = urls[i]
+    ind += 1
+
+
 def openQuest(num):
     global url
 
     driver.get(url)
 
     time.sleep(0.1)
-
-    webpage = driver.page_source
-
 
     questList = driver.find_elements_by_class_name('Counter')
 
@@ -41,7 +66,7 @@ def openQuest(num):
 
 
     texts_mainB = driver.find_element_by_xpath("//div[@class='prob_maindiv']")
-    texts = texts_mainB.find_elements_by_class_name('left_margin')[:-1]
+    texts = texts_mainB.find_elements_by_tag_name('p')
     textsExtra = texts_mainB.find_elements_by_tag_name('i')
 
     superText = ""
@@ -59,19 +84,23 @@ def inputAnswer(ans):
     goFutherButton = driver.find_element_by_xpath("//input[@value='Сохранить']")
     goFutherButton.click()
     possibleTexts = driver.find_elements_by_tag_name('td')
+
+    inNeedOfRightAnswer = False
     for i in possibleTexts:
+        if inNeedOfRightAnswer:
+            inNeedOfRightAnswer=False
+            print('Правильный ответ: ',i.get_attribute('innerHTML')) # Get right answer if we failed to write it
         intrestingProprerty = str(i.get_attribute('style'))
         if intrestingProprerty.startswith('text-align') and 'background' in intrestingProprerty:
-            if ('255, 192, 192' in intrestingProprerty):
-                #we got it wrong my guys...
-                print("Неверно...")
-            else:
+            if ('192, 255, 192)' in intrestingProprerty): # Green color
                 #correct!
                 print("Верно!")
+            else:
+                inNeedOfRightAnswer=True
+                #we got it wrong my guys...
+                print("Неверно...")
         #=='text-align:center;background:#ffc0c0'
 
-
-url = 'https://rus-ege.sdamgia.ru'
 
 print(openQuest(0))
 inputAnswer(input('Ответ: '))
